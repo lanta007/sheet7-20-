3754c926781ae186f25581e924396740a8659131
lanta007 <s6203051613051@email.kmutnb.ac.th>
2022-09-30 11:52:37 +0700

    ninjago

---
+++ src/component/Quiz.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import QuestionsData from "../data/QuestionsData";
+
+const Quiz = ()=>{
+    //console.log(QuestionsData);
+    const [current,setCurrent] = useState(0)
+    return(
+        <div className="quiz">
+            <h1>{QuestionsData[current].question}</h1>
+            <div className="choice">
+                <button>{QuestionsData[current].A}</button>
+                <button>{QuestionsData[current].B}</button>
+                <button>{QuestionsData[current].C}</button>
+                <button>{QuestionsData[current].D}</button>
+            </div>
+        </div>
+    )
+}
+
+export default Quiz;
